d4e67e8c73b0efd6a153a8dc18d69b5372870fcd
Hanuš Valenta <hanus.valenta@protonmail.com>
2022-12-06 22:14:03 +0100

    added button and test text

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import MyButton from './UI/MyButton';
 import './App.css';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main:"#2e1667",
+    },
+    secondary: {
+      main:"#c7d8ed",
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Roboto'
+    ],
+    h4: {
+      fontWeight: 600,
+      fontSize: 28,
+      lineHeight: '2rem',
+      },
+    h5: {
+      fontWeight: 100,
+      lineHeight: '2rem',
+    },
+  },
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+        <MyButton/>
+      </ThemeProvider>
+
+      <p>test2</p>
     </div>
   );
 }
---
+++ src/UI/MyButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from '@material-ui/core'
+
+function MyButton() {
+  return (
+      <Button>Test</Button>
+  )
+}
+
+export default MyButton
