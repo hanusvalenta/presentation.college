dd21ec32fed687c550abb4ff055ca25d2e64da78
Hanuš Valenta <hanus.valenta@protonmail.com>
2022-12-06 22:16:26 +0100

    no custom css

--- src/App.js
+++ src/App.js
@@ -1,38 +1,10 @@
-import { createTheme, ThemeProvider } from '@material-ui/core/styles';
 import MyButton from './UI/MyButton';
 import './App.css';
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main:"#2e1667",
-    },
-    secondary: {
-      main:"#c7d8ed",
-    },
-  },
-  typography: {
-    fontFamily: [
-      'Roboto'
-    ],
-    h4: {
-      fontWeight: 600,
-      fontSize: 28,
-      lineHeight: '2rem',
-      },
-    h5: {
-      fontWeight: 100,
-      lineHeight: '2rem',
-    },
-  },
-});
 
 function App() {
   return (
     <div className="App">
-      <ThemeProvider theme={theme}>
-        <MyButton/>
-      </ThemeProvider>
+      <MyButton/>
 
       <p>test2</p>
     </div>
