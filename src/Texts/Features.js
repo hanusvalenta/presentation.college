62b21f232b4ca26cc3becb7c159a975c490aeb06
Hanuš Valenta <hanus.valenta@protonmail.com>
2022-12-23 01:25:34 +0100

    small text change

--- src/Texts/Features.js
+++ src/Texts/Features.js
@@ -2,25 +2,34 @@ import React from 'react'
 
 function Features() {
   return (
-    <><><><h2 id="Section2">Práce</h2>
+    <><><><><><><><><h2 id="Section2">Práce</h2>
       <p>
         Hra mám implementováno spoustu vlastností a mnoho ji ještě čeká např.
-
-        Různé překážky pasti a nepřátelé, které musí hráč překonat, aby dosáhl svého cíle.
-
-        Vylepšení, které může hráč využít k získání dočasné výhodě ve hře.
-
-        Unikátní prostředí navrhnuté k největší monilitě a velké možnosti pohybu pro hráče.
-
-        Příběhový režim, který sleduje cestu postavy hráče, když se snaží zničit počítačový systém, s cutscénami a dialogy, které pomáhají dotvářet herní svět a postavy.
-
-        Online režimy pro více hráčů, které umožňují hráčům soutěžit proti sobě nebo se spojovat, aby společně čelili výzvám.
-
-        Speciální výzvy nebo cíle, které může hráč splnit, aby získal odměny nebo odemkl nový obsah.
       </p></>
       <h4>Prostředí</h4></>
       <p>
-      více levelů a úrovní s pomalu zvyšující se obtížností, jak hráč postupuje hrou.
+        Více levelů s pomalu se zvyšující obtížností, jak hráč postupuje hrou.
+        A navrhnuté k největším možnostem pohybu pro hráče.
+      </p>
+      <h4>Překážky</h4></>
+      <p>
+        Různé překážky, pasti a nepřátelé, které musí hráč překonávat při průchodu levelem, aby dosáhl svého cíle.
+      </p>
+      <h4>Vylepšení</h4></>
+      <p>
+        Vylepšení, které může hráč využít k získání dočasné výhodě ve hře.
+      </p>
+      <h4>Režimy</h4></>
+      <p>
+        Online režimy pro více hráčů, které umožňují hráčům soutěžit proti sobě nebo se spojovat v herních módech s různými variacemi.
+      </p>
+      <h4>Výzvy</h4></>
+      <p>
+        Speciální výzvy, které může hráč splnit, aby získal odměny nebo odemkl nový obsah.
+      </p>
+      <h4>Arzenál zbraní</h4></>
+      <p>
+        Systém zbraní zahrnující vybavení aby hráči mohli vždy mít možnost zvolit si ideální zbraň pro danou situaci.
       </p></>
   )
 }
--- src/Texts/Optimalization.js
+++ src/Texts/Optimalization.js
@@ -2,19 +2,11 @@ import React from 'react'
 
 function Optimalization() {
   return (
-    <><h2 id="Section3">Shrnutí</h2><p>Zajištění plynulého a citlivého ovládání: Hráči by měli být schopni snadno ovládat postavu trojského koně a procházet herním světem bez jakýchkoli prodlev nebo prodlev.
-
-    Vyvážení obtížnosti: Hra by měla být náročná, ale ne frustrující, s postupným zvyšováním obtížnosti, jak hráč postupuje úrovněmi.
-    
-    Poskytování jasných cílů a úkolů: Hráči by měli vždy vědět, co musí udělat, aby dokončili úroveň nebo porazili nepřítele.
-    
-    Nabídka různého obsahu: Hráči by měli mít možnost si hru užívat po delší dobu tím, že nabídne řadu úrovní, nepřátel a vylepšení, aby věci zůstaly zajímavé.
-    
-    Přidání hodnoty opakování: Hráči by měli být motivováni k tomu, aby si znovu hráli úrovně nebo zkoušeli různé strategie nabízením odměn nebo úspěchů za splnění určitých úkolů.
-    
-    Optimalizace grafiky a výkonu: Hra by měla běžet hladce na různých zařízeních a měla by mít vysoce kvalitní grafiku, která umocní zážitek hráče.
-    
-    Poskytování intuitivního a uživatelsky přívětivého rozhraní: Hráči by měli být schopni snadno procházet menu a možnosti hry bez jakýchkoli zmatků.</p></>
+    <><h2 id="Section3">Shrnutí</h2>
+    <p>
+      Při práci na tomto projektu jsem se dozvěděl spoustu novích věcí o unreal engine zejména o fungování replikace a zejména multiplayeru.
+      
+    </p></>
   )
 }
 
