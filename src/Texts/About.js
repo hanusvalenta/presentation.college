ce16809ea2bcb04e66a6bb151931c303e57c9d5e
Hanuš Valenta <hanus.valenta@protonmail.com>
2022-12-28 16:14:54 +0100

    text changes minimal

--- src/Texts/About.js
+++ src/Texts/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 function About() {
   return (
     <><><h3 id="Section1">Koncept</h3><p>
-      Rád bych představil hru, kterou zatím vyvíjím pod jménem Projekt P. Hru bych charakterizoval jako kooperativní multiplayer FPS s destrukcí. Podstatou této hry je, že se více hráčů společnými silami snaží zničit hlavní objekt (podle zasazení hry) uprostřed mapy. Současně však musí odrážet vlny nebezbečných nepřátel.
+      Rád bych představil hru, kterou zatím vyvíjím pod jménem Projekt P. Hru bych charakterizoval jako kooperativní multiplayer FPS s destrukcí. Podstatou této hry je, že se více hráčů společnými silami snaží zničit hlavní objekt uprostřed mapy. Současně však musí odrážet vlny nebezbečných nepřátel.
     </p></>
     <br></br>
     <p>
--- src/Texts/Features.js
+++ src/Texts/Features.js
@@ -5,7 +5,7 @@ function Features() {
     <><h2 id="Section2">Vlastnosti hry</h2><p>
       Do hry se mi dosud podařilo implementovat několik vlastností a další mám ještě v plánu. Zde je jejich výčet:
     </p><h4>Prostředí</h4><p>
-        Prostředí je navržené tak, aby se mohl hráč pohybovat po mapě v co největším rozsahu, tj. aby mohl jít všude tam, kam chce. V budoucnu bych rád přidal více levelů s pomalu se zvyšující obtížností podle toho, jak hráč postupuje hrou.
+        Prostředí je navržené tak, aby se mohl hráč pohybovat po mapě v co největším rozsahu, tj. aby mohl jít všude tam, kam chce. V budoucnu bych rád přidal více úrovní s pomalu se zvyšující obtížností podle toho, jak hráč postupuje hrou.
       </p><h4>Překážky a pomocné objekty</h4><p>
         Momentálně může hráč skákat; nadále budou do hry zahrnuty různé překážky, pasti a nepřátelé, které musí hráč překonávat při průchodu levelem, aby dosáhl svého cíle. Hráč bude moci také používat řadu pomocných objektů z nabídky, např. dynamit (již částečně implementován), základní vozidla nebo malý jeřáb.
       </p><h4>Arzenál zbraní</h4><p>
